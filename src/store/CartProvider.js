00be241f0941af19e228306b433352fc5990d570
Dario Russo <dariorusso987@gmail.com>
2023-08-23 19:56:09 +0200

    update l180

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -8,11 +8,31 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
   if (action.type === "ADD") {
-    const updateItems = state.items.concat(action.item);
-    const updateTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+    const updateTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+
+    const existingCartItem = state.items[existingCartItemIndex];
+    
+    let updatedItems;
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex]=updatedItem;
+    } else {
+        updatedItems = state.items.concat(action.item);
+    }
+
     return {
-        items: updateItems,
-        totalAmount: updateTotalAmount
+      items: updatedItems,
+      totalAmount: updateTotalAmount,
     };
   }
   return defaultCartState;
@@ -29,7 +49,7 @@ const CartProvider = (props) => {
   };
 
   const removeItemFromCartHandler = (id) => {
-    dispatchCartAction({type: 'REMOVE', id:id});
+    dispatchCartAction({ type: "REMOVE", id: id });
   };
 
   const cartContext = {
